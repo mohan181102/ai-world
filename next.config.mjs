b4d5e104c36c8779a6f5f9cfa2f3c520bd5aefe3
manmohan <manmohan33chandrakar@gmail.com>
2024-02-29 18:11:41 +0530

    g

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-/* ...Your other config rules */
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
