1862c7cf7f4505cb487958efb4678cab73cf0a19
manmohan <manmohan33chandrakar@gmail.com>
2024-02-29 18:48:31 +0530

    change utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,8 +3,8 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
+import { aspectRatioOptions } from "@/constant";
 
-import { aspectRatioOptions } from "@/constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
